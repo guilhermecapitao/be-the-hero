0f08d17ae7f4c58dc9d7b6615e297a8481e397ba
Guilherme Capitão <guilherme.capitao95@gmail.com>
2020-03-24 19:45:40 -0300

    Backend

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors({
+    // origin: 'http://teste.com'
+}))
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333);
